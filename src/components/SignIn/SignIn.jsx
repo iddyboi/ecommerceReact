5eb9d7787f99b57d0735b4d0bfbeee24affea373
Idris <idrist2013@gmail.com>
2020-05-24 14:04:37 +0100

    finishing sign in with email and password

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -3,7 +3,7 @@ import "./styles.scss";
 import FormInput from "../Form-Input/FormInput";
 import CustomButton from "../custom-button/CustomButton";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 export default class SignIn extends Component {
   state = {
@@ -11,9 +11,16 @@ export default class SignIn extends Component {
     password: "",
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: " ", password: "" });
+
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: " ", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = (event) => {
